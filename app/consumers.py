a2a070b0edd0ae1b1a9ea4e9d23f635ebdf23c26
Muhammed Ali <muhamzyali@gmail.com>
2022-07-02 17:50:21 +0100

    group name

--- app/consumers.py
+++ app/consumers.py
@@ -5,14 +5,14 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.chat_box_name = self.scope["url_route"]["kwargs"]["chat_box_name"]
-        self.room_group_name = "chat_%s" % self.chat_box_name
+        self.group_name = "chat_%s" % self.chat_box_name
 
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
 
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -20,7 +20,7 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         username = text_data_json["username"]
 
         await self.channel_layer.group_send(
-            self.room_group_name,
+            self.group_name,
             {
                 "type": "chatroom_message",
                 "message": message,
